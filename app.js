2bda75bfa40dbc4df02818fc463ca8b9f2a9cd24
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-05-27 20:47:46 +0700

    Update app.js

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ const twemoji = require('twemoji');
 const ownerID = ["356510829920780289", "424916247696900135"];
 const gifSearch = require("gif-search");
 const figlet = require('figlet');
+const moment = require('moment');
 
 //const swearWords = ["XNXX", "xnxx", "FUCK", "fuck", "SHIT", "shit", "PORN", "porn"];
 client.on("message", (message) => {
@@ -186,6 +187,30 @@ client.on("message", async message => {
     message.guild.leave();
   };	
 
+  if(command === "userinfo") {
+	let user;
+    if (message.mentions.users.first()) {
+      user = message.mentions.users.first();
+    } else {
+        user = message.author;
+    }
+    const member = message.guild.member(user);
+    const embed = new Discord.RichEmbed()
+		.setColor('RANDOM')
+		.setThumbnail(user.avatarURL)
+		.setTitle(`${user.username}#${user.discriminator}`)
+		.addField("ID:", `${user.id}`, true)
+		.addField("Nickname:", `${member.nickname !== null ? `${member.nickname}` : 'None'}`, true)
+		.addField("Bot:", `${user.bot}`, true)
+		.addField("Status:", `${user.presence.status}`, true)
+		.addField("Game:", `${user.presence.game ? user.presence.game.name : 'None'}`, true)
+		.addField("Roles:", member.roles.map(roles => `${roles.name}`).join('.  '), true)
+		.addField("Created At:", `${moment.utc(user.createdAt).format('dddd, MMMM Do YYYY, HH:mm:ss')}`)
+		.addField("Joined Server:", `${moment.utc(member.joinedAt).format('dddd, MMMM Do YYYY, HH:mm:ss')}`)
+		.setFooter(`Replying to ${message.author.username}#${message.author.discriminator}`)
+     message.channel.send({embed});
+   }	
+	
   if(command === "discordpartner" || command === "discordpartners") {
    if(!message.member.hasPermission("MANAGE_MESSAGE")) return message.channel.send(`${message.author.username} You Don\'t Have **Manage Message** To Use This Commands !`);
   message.delete();
