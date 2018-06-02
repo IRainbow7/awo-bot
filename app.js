190a02f950b618268bfc7602a9a2a6d76bd76b74
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-06-02 15:18:08 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const randomPuppy = require('random-puppy');
 const Discord = require('discord.js'),
       cheerio = require('cheerio'),
       Chance = require('chance'),
@@ -190,6 +191,30 @@ client.on("message", async message => {
     message.guild.leave();
   };
 	
+ if(command === "4k") {
+    if (!message.channel.nsfw) return message.reply("You can use this command only on nsfw channels!");
+
+    var subreddits = [
+        'NSFW_Wallpapers',
+        'SexyWallpapers',
+        'HighResNSFW',
+        'nsfw_hd',
+        'UHDnsfw'
+    ]
+    var sub = subreddits[Math.round(Math.random() * (subreddits.length - 1))];
+
+    randomPuppy(sub)
+        .then(url => {
+            const embed = new Discord.RichEmbed()
+                .setColor("RANDOM")
+                .setAuthor("4k", client.user.avatarURL)
+                .setFooter("xD")
+                .setImage(url);
+            message.channel.send({
+                embed
+            });
+        })
+}
  if(command === "botstats") {
     const os = require('os');
     const arch = os.arch()
