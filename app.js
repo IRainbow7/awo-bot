f255250ae44fcfa52a783215549e84c6a60a0156
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-06-03 21:07:00 -0400

    Update app.js

--- app.js
+++ app.js
@@ -236,6 +236,34 @@ client.on("message", async message => {
     message.guild.leave();
   };
 	
+if(command === "banslist") {
+	let ban = await message.guild.fetchBans().catch(error => {
+        return message.channel.send('Sorry, I don\'t have the proper permissions to view bans!');
+    });
+
+    ban = ban.array();
+    let users = message.guild.fetchBans().id;
+
+    arraySort(ban, 'size', {
+        reverse: true
+    });
+
+    let possiblebans = [
+        ['Users', 'ID']
+    ];
+    ban.forEach(function(ban) {
+        possiblebans.push([ban.username, ban.id]);
+    })
+
+    const embed = new Discord.RichEmbed()
+        .setColor(0xCB5A5E)
+        .addField('Bans', `\`\`\`${table.table(possiblebans)}\`\`\``);
+    send(message.channel, embed, {
+        name: 'bans',
+        icon: 'http://gaia.adage.com/images/bin/image/x-large/iStock47643841422.jpg'
+    });
+};
+	
  if(command === "botstats") {
     const os = require('os');
     const arch = os.arch()
